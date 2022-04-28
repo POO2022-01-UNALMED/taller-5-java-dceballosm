e516be7bd8df138cfe547d7f2dc0d63fbd31d4e8
Daniel Ceballos <dceballosm@unal.edu.co>
2022-04-27 19:05:30 -0500

    Create Zona.java

---
+++ src/main/java/gestion/Zona.java
@@ -0,0 +1,42 @@
+package gestion;
+
+import java.util.ArrayList;
+import zooAnimales.*;
+
+public class Zona {
+   private String nombre;
+   private Zoologico zoo;
+   private ArrayList<Animal> animales = new ArrayList();
+
+   public Zona(String nombre, Zoologico zoo) {
+      this.nombre = nombre;
+      this.zoo = zoo;
+   }
+
+   public Zona() {
+   }
+
+   public String getNombre() {
+      return this.nombre;
+   }
+
+   public Zoologico getZoo() {
+      return this.zoo;
+   }
+
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
+
+   public void setZoo(Zoologico zoo) {
+      this.zoo = zoo;
+   }
+
+   public void agregarAnimales(Animal animal) {
+      this.animales.add(animal);
+   }
+
+   public int cantidadAnimales() {
+      return this.animales.size();
+   }
+}
